401eb58018f46733ecfb5a1df389e99f1be70dbc
bryson.sol <97324072+LoneStarDev@users.noreply.github.com>
2022-04-19 14:11:24 -0700

    Formatting and service icon addition

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
+// Carousel Below....
 
-   const isPhantomInstalled = window.solana && window.solana.isPhantom
-   const getProvider = () => {
-       if ("solana" in window) {
-         const provider = window.solana;
-         if (provider.isPhantom) {
-           return provider;
-         }
-       }
-       window.open("https://phantom.app/", "_blank");
-     };
+var slideIndex = 1;
+showSlides(slideIndex);
 
-
-function getAccount() {
-    window.solana.connect
-    window.solana.request({method:"connect"})
-    window.solana.on("connect", () => console.log("connected!"))
-
-
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function disconnectAccount() {
-    window.solana.disconnect()
-    window.solana.on('disconnect'),
-    () => 
-    console.log("disconnected!")
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Carousel End
+
+
+
+
